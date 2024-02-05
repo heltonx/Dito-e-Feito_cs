da7b96663dcdec60fe545e8c22eba9348529dd27
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2024-02-05 19:14:24 -0300

    Update tasks.cs
    adding text

--- tasks.cs
+++ tasks.cs
@@ -1,16 +1,30 @@
+/*
+katas for the celebred microsoft language
+
+based on classes with friend Miguel. Aand thanks to teacher Não Importa
+*/
+
+
+/* DITO 1 - HELLO WORLD*/
+// ======================================================================
+
 using System;
-
-
-namespace tantofaz {
-	
+namespace tantofaz 
+{
 		class program {
-			
 			static void Main (string[] args)
 			{
 				Console.WriteLine("Hello World");
 			}
-			
-			
 		}
-	
 }
+
+
+/*
+Compile: C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:gatherer.exe gatherer.cs
+
+Obs.: check if the .NET version installed at your computer is really like this example, if not, get the right path.
+*/
+
+
+// ======================================================================
